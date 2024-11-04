9bb43f1457da561c1282e9a984b4ec636754c81e
BleuSkell <kyasowi1@gmail.com>
2024-11-04 12:06:46 +0100

    finished lessons 24 and 25 of laravel 30 days

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Job;
 use App\Models\User;
+use App\Mail\JobPosted;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -36,11 +38,15 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
     
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->queue(
+            new JobPosted($job)
+        );
     
         return redirect('/jobs');
     }
